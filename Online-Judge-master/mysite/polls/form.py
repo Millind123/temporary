b4817961f6655d72982408493cdf3f1a562db004
Millind123 <91657098+Millind123@users.noreply.github.com>
2022-07-05 15:07:49 +0530

    Delete Online-Judge-master/mysite directory

--- Online-Judge-master/mysite/polls/form.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class NameForm(forms.Form):
-    code = forms.CharField(label='code', max_length=100)
-    problemID = forms.IntegerField()
--- Online-Judge-master/mysite/polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:number>/', views.problem_statement, name='problemStatement'),
-    path('result/', views.get_code, name='result'),
-]
